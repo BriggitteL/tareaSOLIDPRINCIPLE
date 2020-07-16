320d22424ffe0e2e5a59886d240d028f7f0f765f
Briggitte Lopez <brililop@espol.edu.ec>
2020-07-16 18:44:57 -0500

    Codigo para tarea de solidos

---
+++ inmobiliaria/src/Persona/Administrador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persona;
+
+import Persona.Usuario;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Administrador extends Usuario{
+
+    // Constructor que extiende de Usuario
+    public Administrador(String nombre, String correoElectronico, String contrasena, String rol) {
+        super(nombre, correoElectronico, contrasena, rol);
+    }
+    
+}
